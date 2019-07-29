3e29e1a530a269bfbf1a8e5388a5f4900557f54b
Your Name <Matthewjdemeo@knights.ucf.edu>
2019-07-28 22:57:13 -0400

    Challenge build

--- SpaceShooter/Assets/Scripts/GameController.cs
+++ SpaceShooter/Assets/Scripts/GameController.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-   public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
-    public Text ScoreText;
+    public Text scoreText;
     private int score;
     public Text restartText;
     public Text gameOverText;
+    public Text createdText;
+    public Text winText;
     private bool gameOver;
     private bool restart;
-
+    
     void Start ()
    {
     gameOver = false;
     restart = false;
     restartText.text = "";
     gameOverText.text = "";
+    winText.text = "";
+    createdText.text = "";
     score = 0;
     UpdateScore();
     StartCoroutine (SpawnWaves ());
@@ -33,9 +38,9 @@ public class GameController : MonoBehaviour
    {
        if (restart)
        {
-           if(Input.GetKeyDown (KeyCode.R))
+           if(Input.GetKeyDown (KeyCode.F))
            {
-               Application.LoadLevel(Application.loadedLevel);
+               SceneManager.LoadScene("SpaceShooter");
            }
        }
    }
@@ -47,6 +52,7 @@ public class GameController : MonoBehaviour
     {
         for (int i = 0; i < hazardCount; i++)
         {
+            GameObject hazard = hazards[Random.Range (0,hazards.Length)];
             Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
             Instantiate (hazard, spawnPosition, spawnRotation);
@@ -56,7 +62,7 @@ public class GameController : MonoBehaviour
 
         if(gameOver)
         {
-            restartText.text = "Press 'R' for Restart";
+            restartText.text = "Press 'F' for Restart";
             restart = true;
             break;
         }
@@ -71,9 +77,15 @@ UpdateScore();
 
 void UpdateScore()
 {
-ScoreText.text = "Score: " + score;
-}
-
+scoreText.text = "Points: " + score;
+  if (score >= 100)
+          {
+            winText.text = "You win!";
+            createdText.text = "Created by Matthew DeMeo!";
+            gameOver = true;
+            restart = true;
+           }
+      }
 public void GameOver()
 {
     gameOverText.text = "Game Over!";
@@ -81,3 +93,5 @@ public void GameOver()
 }
 
 }
+
+
